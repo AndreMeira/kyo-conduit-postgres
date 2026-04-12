47b876a9b5d78d606b19e923df95b34dbbe7e6ca
AndreMeira <a.meira92@gmail.com>
2026-04-13 00:19:37 +0200

    use cases

---
+++ src/main/scala/conduit/domain/service/usecase/UseCases.scala
@@ -0,0 +1,49 @@
+package conduit.domain.service.usecase
+
+import conduit.domain.service.persistence.Database.Transaction
+
+/** Aggregates all use cases in the application, following the same pattern as
+  * [[conduit.domain.service.persistence.Persistence]] for repositories.
+  *
+  * This class provides a single access point to all use cases, making it easy
+  * to pass them as a dependency to the HTTP layer or any other consumer.
+  *
+  * @tparam Tx the transaction type used for database operations
+  *
+  * @param userRegistration    registers a new user
+  * @param userAuthentication  authenticates an existing user
+  * @param userRead            retrieves the current user
+  * @param userUpdate          updates the current user
+  * @param profileRead         retrieves a user profile
+  * @param profileFollowing    follows a user profile
+  * @param profileUnfollowing  unfollows a user profile
+  * @param articleCreation     creates a new article
+  * @param articleRead         retrieves a single article
+  * @param articleUpdate       updates an existing article
+  * @param articleFeed         retrieves articles from followed users
+  * @param listArticles        lists articles with filters
+  * @param articleFavorite     favorites an article
+  * @param articleUnfavorite   unfavorites an article
+  * @param commentAddition     adds a comment to an article
+  * @param commentDeletion     deletes a comment
+  * @param listComments        lists comments for an article
+  */
+class UseCases[Tx <: Transaction](
+  val userRegistration: UserRegistrationUseCase[Tx],
+  val userAuthentication: UserAuthenticationUseCase[Tx],
+  val userRead: UserReadUseCase[Tx],
+  val userUpdate: UserUpdateUseCase[Tx],
+  val profileRead: ProfileReadUseCase[Tx],
+  val profileFollowing: ProfileFollowingUseCase[Tx],
+  val profileUnfollowing: ProfileUnfollowingUseCase[Tx],
+  val articleCreation: ArticleCreationUseCase[Tx],
+  val articleRead: ArticleReadUseCase[Tx],
+  val articleUpdate: ArticleUpdateUseCase[Tx],
+  val articleFeed: ArticleFeedUseCase[Tx],
+  val listArticles: ListArticlesUseCase[Tx],
+  val articleFavorite: ArticleFavoriteUseCase[Tx],
+  val articleUnfavorite: ArticleUnfavoriteUseCase[Tx],
+  val commentAddition: CommentAdditionUseCase[Tx],
+  val commentDeletion: CommentDeletionUseCase[Tx],
+  val listComments: ListCommentsUseCase[Tx],
+)
